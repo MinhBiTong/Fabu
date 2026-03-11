66dcc876cbb753a5ce09ac078b41965fb5d58bef
DeterToleration <Ntduong100205@gmail.com>
2026-03-11 18:12:36 +0700

    Sign in base
    cái login form tạm thời (tui chưa biết cacah các ông cho đăng nhập hoặc đăng ký kiểu gì ) (ấn sign in để mở log in form)

---
+++ src/frontend/src/components/features/auth/LoginForm.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+type Props = {
+  onClose: () => void;
+};
+
+function LoginForm({ onClose }: Props) {
+  return (
+    <div className= "FullScreen">
+    <div className="loginOverlay">
+
+      <div className="loginform">
+
+        <button className="closeBtn"onClick={onClose}>✕</button>
+
+        <h1>Sign In</h1>
+
+       <div className="Mainform">
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+
+        <button className="loginSubmit">
+          Login
+        </button>
+           </div>
+      </div>
+
+    </div>
+    </div>
+  );
+}
+export default LoginForm;
--- src/frontend/src/components/layout/Header.tsx
+++ src/frontend/src/components/layout/Header.tsx
@@ -1,18 +1,28 @@
+"use client";
+
+import { useState } from "react";
 import Image from "next/image";
+
+import Loginform from "../features/auth/LoginForm";
+
 import logo from "../../styles/images/FABUlogo.png";
 import Icon from "../../styles/images/search.png";
 import Menu from "../../styles/images/menu.png";
 import User from "../../styles/images/user.png";
 
 function Header() {
+
+    const [showLogin, setShowLogin] = useState(false);
+    
   return (
+    <>
     <div className="navibar">
 
       <div className="menu">
-        <Image src={Menu} alt="Menu"  />
+      <Image src={Menu} alt="Menu"  />
       </div>
       
-      <div className="logo">
+       <div className="logo">
         <Image src={logo} alt="Logo"  />
       </div>
 
@@ -22,13 +32,12 @@ function Header() {
         <button>Serasdasdvices</button>
         <button>Contasdasdact</button>
       </div>
-
       <div className="signin">
       <button className="SearchButton">
         <Image src={Icon} alt="Search" width={20} height={20} />
       </button >
      
-        <button className="SigninButton">Sign in</button>
+        <button className="SigninButton"   onClick={() => setShowLogin(true)}>Sign in</button>
          <button className="ProfileButton">
            <Image src={User} alt="Search"/>
          </button>
@@ -36,6 +45,11 @@ function Header() {
       </div>
 
     </div>
+      {showLogin && (
+        <Loginform onClose={() => setShowLogin(false)} />
+      )}
+
+    </>
   );
 }
 
