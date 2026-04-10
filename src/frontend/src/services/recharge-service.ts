2938673c74d813c420f0c3a5c835c7db007a42b0
chau <nguyenkhanhchau51592k5@gmail.com>
2026-04-10 10:33:12 +0700

    bo sung logic call api recharge

--- src/frontend/src/services/recharge-service.ts
+++ src/frontend/src/services/recharge-service.ts
@@ -1,1 +1,14 @@
 // đóng vai trò là các HttpClient đơn thuần, chỉ gọi API và trả về dữ liệu. Logic xử lý dữ liệu phức tạp nên đẩy về Custom Hooks.
+import { globalApiClient } from "../app/api/ApiClient";
+
+export const getPackages = async () => {
+  return await globalApiClient.get<any>("recharge/packages");
+};
+
+export const rechargeApi = async (payload: {
+  phone: string;
+  amount: number;
+  coupon: string;
+}) => {
+  return await globalApiClient.post<any>("recharge", payload);
+};
